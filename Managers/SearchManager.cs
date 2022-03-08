debd85a8ed111ca868c9f91d05598a6e2f3abcad
SU <bamboo2323231@gmail.com>
2022-03-08 22:50:37 +0800

    SU Plus a searchManager and EmptyFunction

--- Managers/SearchManager.cs
+++ Managers/SearchManager.cs
@@ -11,15 +11,19 @@ namespace MKForum.Managers
     public class SearchManager
     {
 
-        public static List<Post> getAllSrchList(string srchText, int cboardid, List<Post> srchPostList)
+        public static List<SearchResult> getAllSrchList(string srchText, int cboardid, List<SearchResult> srchList)
         {
 
-
+            //找 文章標題 或 文章內容 或 作者名稱 或 子板塊名稱 (SQL已測試OK)
             string connStr = "Server=localhost;Database=MKForum;Integrated Security=True;";            //連線字串
             string commandText = @"
-                        SELECT *
+                        SELECT [Cboards].[CboardID],[Cboards].[Cname],[Posts].[PostID],[Posts].Title,[Posts].PostCotent, [Members].[NickName]
                         FROM [MKForum].[dbo].[Posts]
-            WHERE PostCotent LIKE '%@srchWord%' OR Title LIKE '%@srchWord%'
+                        INNER JOIN [Members]
+                        ON [MKForum].[dbo].[Posts].[MemberID] = [MKForum].[dbo].[Members].[MemberID]
+			            INNER JOIN [Cboards]
+			            ON [MKForum].[dbo].[Cboards].[CboardID] = [MKForum].[dbo].[Posts].[CboardID]
+			            WHERE[PostCotent] LIKE '%@srchText%' OR [Title] LIKE '%@srchText%' OR [NickName] LIKE '%@srchText%' OR [Cname] LIKE '%@srchText%';
                         ";
 
             try
@@ -28,7 +32,7 @@ namespace MKForum.Managers
                 {
                     using (SqlCommand command = new SqlCommand(commandText, conn))
                     {
-                        command.Parameters.AddWithValue("@srchWord", srchText);
+                        command.Parameters.AddWithValue("@srchText", srchText);
 
                         conn.Open();
                         SqlDataReader reader = command.ExecuteReader();
@@ -36,15 +40,19 @@ namespace MKForum.Managers
                         //把取得的資料放進陣列(不知道為什麼讀不到)
                         while (reader.Read())
                         {
-                            Post MatchData = new Post()
+                            SearchResult MatchData = new SearchResult()
                             {
                                 PostID = (Guid)reader["PostID"],
+                                NickName = reader["NickName"]as string,
                                 Title = reader["Title"] as string,
+                                PostCotent = reader["PostCotent"] as string,
+                                Cname = reader["Title"] as string,
+                                CboardID = (Guid)reader["CboardID"],
                             };
-                            srchPostList.Add(MatchData);
+                            srchList.Add(MatchData);
 
                         }
-                        return srchPostList;
+                        return srchList;
                     }
                 }
             }
@@ -56,16 +64,21 @@ namespace MKForum.Managers
         }
 
         //取得當前子版搜尋的List
-        public static List<Post> getCboardSrchList(string srchText, int cboardid, List<Post> srchPostList)
+        public static List<SearchResult> getCboardSrchList(string srchText, int cboardid, List<SearchResult> srchList)
         {
-            // 從Session取得當前子板塊ID
+            //從Session取得當前子板塊ID
             //int cboardid = this.Session["CboradID"] as int;
 
+            //找當前子板塊內的 文章標題 或 文章內容 或 作者名稱 (SQL已測試OK)
             string connStr = "Server=localhost;Database=MKForum;Integrated Security=True;";            //連線字串
             string commandText = @"
-                        SELECT *
+                        SELECT [Cboards].[Cname],[Posts].[PostID],[Posts].Title,[Posts].PostCotent, [Members].[NickName]
                         FROM [MKForum].[dbo].[Posts]
-            WHERE CboardID='%@cboardid' PostCotent LIKE '%@srchWord%' OR Title LIKE '%@srchWord%'
+                        INNER JOIN [Members]
+                        ON [Posts].[MemberID] = [Members].[MemberID]
+			            INNER JOIN [Cboards]
+			            ON [Cboards].[CboardID] = [Posts].[CboardID]
+			            WHERE ([PostCotent] LIKE '%@srchText%' OR [Title] LIKE '%@srchText%' OR [NickName] LIKE '%@srchText%') AND [Cname] = '@CboradID';
                         ";
 
             try
@@ -74,22 +87,27 @@ namespace MKForum.Managers
                 {
                     using (SqlCommand command = new SqlCommand(commandText, conn))
                     {
-                        command.Parameters.AddWithValue("@srchWord", srchText);
+                        command.Parameters.AddWithValue("@srchText", srchText);
                         command.Parameters.AddWithValue("@cboardid", cboardid);
 
                         conn.Open();
                         SqlDataReader reader = command.ExecuteReader();
 
-
                         //把取得的資料放進陣列
                         while (reader.Read())
                         {
-                            Post MatchData = new Post()
-                            { PostID = (Guid)reader["PostID"] };
-                            srchPostList.Add(MatchData);
-
+                            SearchResult MatchData = new SearchResult()
+                            {
+                                PostID = (Guid)reader["PostID"],
+                                NickName = reader["NickName"] as string,
+                                Title = reader["Title"] as string,
+                                PostCotent = reader["PostCotent"] as string,
+                                Cname = reader["Title"] as string,
+                                CboardID = (Guid)reader["CboardID"],
+                            };
+                            srchList.Add(MatchData);
                         }
-                        return srchPostList;
+                        return srchList;
                     }
                 }
             }
@@ -100,16 +118,19 @@ namespace MKForum.Managers
             }
         }
         //取得作者搜尋的List
-        public static List<Post> getWriterSrchList(string srchText, int cboardid, List<Post> srchPostList)
+        public static List<SearchResult> getWriterSrchList(string srchText, int cboardid, List<SearchResult> srchList)
         {
-
 
             string connStr = "Server=localhost;Database=MKForum;Integrated Security=True;";            //連線字串
             string commandText = @"
-                SELECT *
-                FROM [MKForum].[dbo].[Posts]
-				WHERE PostCotent LIKE '%@srchWord%' OR Title LIKE '%@srchWord%'
-                ";
+                        SELECT [Cboards].[CboardID],[Cboards].[Cname],[Posts].[PostID],[Posts].Title,[Posts].PostCotent, [Members].[NickName]
+                        FROM [MKForum].[dbo].[Posts]
+                        INNER JOIN [Members]
+                        ON [MKForum].[dbo].[Posts].[MemberID] = [MKForum].[dbo].[Members].[MemberID]
+			            INNER JOIN [Cboards]
+			            ON [MKForum].[dbo].[Cboards].[CboardID] = [MKForum].[dbo].[Posts].[CboardID]
+			            WHERE[NickName] LIKE '%@srchText%';
+                        ";
 
             try
             {
@@ -117,21 +138,26 @@ namespace MKForum.Managers
                 {
                     using (SqlCommand command = new SqlCommand(commandText, conn))
                     {
-                        command.Parameters.AddWithValue("@srchWord", srchText);
+                        command.Parameters.AddWithValue("@srchText", srchText);
 
                         conn.Open();
                         SqlDataReader reader = command.ExecuteReader();
 
-
                         //把取得的資料放進陣列
                         while (reader.Read())
                         {
-                            Post MatchData = new Post()
-                            { PostID = (Guid)reader["PostID"] };
-                            srchPostList.Add(MatchData);
-
+                            SearchResult MatchData = new SearchResult()
+                            {
+                                PostID = (Guid)reader["PostID"],
+                                NickName = reader["NickName"] as string,
+                                Title = reader["Title"] as string,
+                                PostCotent = reader["PostCotent"] as string,
+                                Cname = reader["Title"] as string,
+                                CboardID = (Guid)reader["CboardID"],
+                            };
+                            srchList.Add(MatchData);
                         }
-                        return srchPostList;
+                        return srchList;
                     }
                 }
             }
---
+++ Models/SearchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MKForum.Models
+{
+    public class SearchResult
+    {
+        public Guid PostID { get; set; }
+        public string NickName { get; set; }
+        public string Title { get; set; }
+        public string PostCotent { get; set; }
+        public string Cname { get; set; }
+        public Guid CboardID { get; set; }
+    }
+}
