72f4ea8d075bb37a77bc23dd8bed2ee71229b411
SU <bamboo2323231@gmail.com>
2022-03-09 23:53:19 +0800

    板塊

---
+++ BackAdmin/common area.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MKForum.BackAdmin
+{
+    public partial class common_area : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
